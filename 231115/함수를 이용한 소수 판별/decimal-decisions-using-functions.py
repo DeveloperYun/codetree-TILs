6471954b23d3fa6f46ac3868c44b97a3d0ec602a
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-15 23:48:27 +0900

    Title: 함수를 이용한 소수 판별, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231115/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,18 @@
+# 소수 판별 함수(2이상의 자연수에 대하여)
+def is_prime_number(x):
+    # 2부터 (x - 1)까지의 모든 수를 확인하며
+    for i in range(2, x):
+        # x가 해당 수로 나누어떨어진다면
+        if x % i == 0:
+            return False # 소수가 아님
+    return True # 소수임
+
+s=0
+def answer(a,b):
+    global s
+    for i in range(a,b+1):
+        if is_prime_number(i):
+            s+=i
+a,b=map(int,input().split())
+answer(a,b)
+print(s)
