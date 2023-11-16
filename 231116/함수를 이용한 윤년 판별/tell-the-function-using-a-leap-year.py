6fa20b593aff792c222b3d3c09f96b17aca34b05
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-16 16:06:29 +0900

    Title: 함수를 이용한 윤년 판별, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231116/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 231116/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,8 +1,11 @@
 def is_onjeonsu(n):
-    if n % 4 == 0:
-        return True
+
     if n % 4 == 0 and n%100 == 0:
         return False
+        
+    if n % 4 == 0:
+        return True
+    
     if n % 4 == 0 and n%100 == 0 and n%400 == 0:
         return True
     return False
