cdce07cd5b13c135dcd8cc3868120b4fc6a0736a
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-17 21:17:26 +0900

    Title: 삼각형 만들기, Time: 189ms, Memory: 28MB, Status: Passed - CodeTree

--- 231117/삼각형 만들기/create-triangle.py
+++ 231117/삼각형 만들기/create-triangle.py
@@ -1,4 +1,3 @@
-import math
 n=int(input())
 
 points=[]
@@ -13,9 +12,10 @@ def solution(a,b,c):
     t = 0
     #한 면은 x축에 평행 y축에 평행
     if (y1==y2 or y1==y3 or y2==y3) and (x1==x2 or x2==x3 or x1==x3):
-        t = 0.5*abs((x1*y2+x2*y3+x3*y1)-(x2*y1+x3*y2+x1*y3))
+        t = abs((x1*y2+x2*y3+x3*y1)-(x2*y1+x3*y2+x1*y3))
     
     return t
+
 ans=0
 for i in range(n):
     for j in range(i+1,n):
@@ -25,4 +25,4 @@ for i in range(n):
             if temp > ans:
                 ans = temp
 
-print(math.ceil(ans)*2)
+print(int(ans))
