1f06c4be663cecc3482cb342669b84dba0b1b69b
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-17 23:46:17 +0900

    Title: 사칙연산 함수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231117/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,12 @@
+a,b,c=map(str,input().split())
+a = int(a)
+c = int(c)
+
+if b=="+":
+    print(a,"+",c,"=",a+c)
+elif b=="-":
+    print(a,"-",c,"=",a-c)
+elif b=="/":
+    print(a,"/",c,"=",a/c)
+else:
+    print(a,"*",c,"=",a*c)
