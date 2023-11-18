20a8635a735cf2fc4a98542d404f53fd74c4b0b3
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-18 23:16:03 +0900

    Title: 겹치지 않는 선분 2, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 231118/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 231118/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -7,15 +7,18 @@ for i in range(n):
 cnt=0
 
 for i in range(n):
+    overlap=False
     for j in range(n):
         if i==j:
             continue
 
-        a1,a2=arr[i] #3,9
-        b1,b2=arr[j] #7,8
+        a1,a2=arr[i] #x[i][0],x[i][1]
+        b1,b2=arr[j] #x[j][0],x[j][1]
 
-        if a1<b2 and a2>b1 and b2>a2:
-            
-            cnt += 1
+        if (a1<=b1 and a2>=b2) or (a1>=b1 and b2>a2):
+            overlap = True
+            break
+    if overlap == False:
+        cnt += 1
 
 print(cnt)
