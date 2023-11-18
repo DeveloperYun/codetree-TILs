92ad5f093186224d34d78141cb83eece8201dca0
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-19 00:52:48 +0900

    Title: 이상한 폭탄 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231119/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,14 @@
+n,k=map(int,input().split())
+arr=[]
+for i in range(n):
+    t=int(input())
+    arr.append(t)
+
+answer=[]
+for i in range(n):
+    for j in range(i+1,n):
+        if arr[i] == arr[j]:
+            if j-i <= k:
+                answer.append(arr[i])
+
+print(max(answer))
