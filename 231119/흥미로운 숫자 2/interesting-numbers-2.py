e01989edd868de2e92de533903ae268832085273
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-19 23:33:42 +0900

    Title: 흥미로운 숫자 2, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231119/흥미로운 숫자 2/interesting-numbers-2.py
+++ 231119/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,16 +1,33 @@
-x, y = map(int, input().split())
+x,y=map(int,input().split())
 
-cnt = 0
-for i in range(x, y+1):
+cnt=0
 
-    n_arr = list(map(int, list(str(i))))
-    n_dic = {}
-    for n in n_arr:
-        if n in n_dic:
-            n_dic[n] += 1
-        else:
-            n_dic[n] = 1
-    if len(n_dic) == 2 and 1 in n_dic.values():
-        #print(i)
-        cnt += 1
+def funny(n):
+    temp = list(map(int,str(n)))
+    
+    #전부 같은 경우
+    t=temp[0]
+    if temp.count(t) == len(temp):
+        return False
+    
+    #한 자리 이상 다른 경우
+    #다른거 두개 찾아서 count 때리면 되나?
+    for i in range(len(temp)):
+        for j in range(i+1,len(temp)):
+            if temp[i] != temp[j]:
+                c1 = temp.count(temp[i]) #1
+                c2 = temp.count(temp[j]) #3
+
+                #11 3
+                if c1 > 1 and c2 == 1:
+                    return True
+                elif c1 == 1 and c2 > 1:
+                    return True
+                else:
+                    return False
+    return True
+for i in range(x,y+1):
+    if funny(i):
+        cnt+=1
+
 print(cnt)
