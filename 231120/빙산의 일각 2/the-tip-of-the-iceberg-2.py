403278b288880a01a03e1a475d08c84786734ef9
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-20 16:18:09 +0900

    Title: 빙산의 일각 2, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

--- 231120/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 231120/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -9,9 +9,12 @@ end=max(h)
 ans=0
 for i in range(start,end):
     # i = 해수면의 높이
-    cnt = 1
-    for j in range(n-1):
-        if h[j] - i > 0 and h[j+1]-i == 0:
+    cnt = 0
+    if h[0] > i:
+        cnt += 1
+
+    for j in range(1,n):
+        if h[j] > i and h[j-1] <= i:
             cnt += 1
     
     ans = max(ans,cnt)
