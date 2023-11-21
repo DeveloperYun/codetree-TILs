49ae59cbdef2c0aa35944f83160efea72207798a
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-22 00:17:12 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 108ms, Memory: 25MB, Status: Passed - CodeTree

--- 231122/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ 231122/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -1,8 +1,8 @@
 a,b,c=map(int,input().split())
 answer=0
 
-for i in range(1,1001):
-    for j in range(1,1001):
+for i in range(1001):
+    for j in range(1001):
         if a*i + b*j <= c:
             answer = max(answer,a*i + b*j)
 
