d80f6fadaf0581c3a77998788a893add23b98c29
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-22 01:35:05 +0900

    Title: 3개의 선 2, Time: 111ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231122/3개의 선 2/three-lines-2.py
+++ 231122/3개의 선 2/three-lines-2.py
@@ -1,37 +1,36 @@
 from itertools import combinations
 import sys
 
-n=int(input())
+n = int(input())
 points = []
-xp=[]
-yp=[]
-tp=[]
+xp = []
+yp = []
+tp = []
 for _ in range(n):
-    x,y=map(int,input().split())
-    points.append((x,y))
-    xp.append(('x',x))
-    yp.append(('y',y))
-    
-xp=list(set(xp))
-yp=list(set(yp))
+    x, y = map(int, input().split())
+    points.append((x, y))
+    xp.append(('x', x))
+    yp.append(('y', y))
+
+xp = list(set(xp))
+yp = list(set(yp))
 tp = xp + yp
 
+temp = list(combinations(tp, 3))
+found = False
 
-
-temp = list(combinations(tp,3))
 for t in temp:
-    # t = ('x', 1), ('x', 0), ('x', 2)
-    cnt=n
+    cnt = n
     for xory, p in t:
-        # 'x', 1
         for point in points:
-            if point[0]==p or point[1]==p:
+            if point[0] == p or point[1] == p:
                 cnt -= 1
-    
-            
-    if cnt<0:
-        print("1")
-        sys.exit(0)
-    else:
-        print("0")
-        sys.exit(0)
+
+    if cnt == 0:
+        found = True
+        break
+
+if found:
+    print("1")
+else:
+    print("0")
