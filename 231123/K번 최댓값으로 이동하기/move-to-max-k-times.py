4978cdcd5be63c003e414315bffb162c26def5c6
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-11-23 14:19:32 +0900

    Title: K번 최댓값으로 이동하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231123/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 231123/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -1,47 +1,95 @@
 from collections import deque
-n, k = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(n)]
-visited = [[0 for _ in range(n)] for _ in range(n)]
-q = deque([])
-dxs, dys = [1,0,-1,0], [0,1,0,-1]
-r, c = map(int,input().split())
-r, c = r - 1, c - 1
-key = 0
 
-def in_range(x,y):
-    return 0 <= x and x < n and 0 <= y and y < n
+n, k = list(map(int ,input().split()))
 
-def can_go(x,y):
-    return in_range(x,y) and visited[x][y] == 0 and graph[x][y] < key
+arr=[
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+r, c = list(map(int, input().split()))
+
+temp = [
+    [e for e in row]
+    for row in arr
+]
+
+visited=[
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+q = deque()
+
+def initialization():
+    global temp
+    global visited
+
+    temp = [
+        [e for e in row]
+        for row in arr
+    ]
+
+    visited=[
+        [0 for _ in range(n)]
+        for _ in range(n)
+    ]
+
+    q = deque()
+
+
+def in_range(y, x):
+    return 0<=y and y<n and 0<=x and x<n
+
+
+def can_go(y, x):
+    global temp
+    return in_range(y, x) and not visited[y][x] and temp[y][x]
 
 def bfs():
-    global MAX
+
+    dys, dxs = [1, 0, -1, 0], [0, 1, 0, -1]
+
     while q:
-        x, y = q.popleft()
-        for dx, dy in zip(dxs,dys):
-            ndx, ndy = x + dx, y + dy
-            if can_go(ndx, ndy):
-                q.append([ndx,ndy])
-                visited[ndx][ndy] = 1
-                MAX = max(MAX,graph[ndx][ndy])
+        y, x = q.popleft()
+        
 
-check_break = True
-for _ in range(k):
-    MAX = 0
-    check = False
-    q.append([r,c])
-    key = graph[r][c]
+        for dy, dx in zip(dys, dxs):
+            ny, nx = y + dy, x + dx
+            if can_go(ny, nx):
+                q.append((ny, nx))
+                visited[y][x] = 1
+
+def find_next(y, x):
+    global temp
+    global visited
+    global q
+
+    candidates=[]
+
+    e = temp[y][x]
+    for j in range(n):
+        for i in range(n):
+            if temp[j][i] >= e:
+                temp[j][i] = 0
+    
+    q.append((y, x))
     bfs()
-    for i in range(n):
-        for j in range(n):
-            if not check and graph[i][j] == MAX:
-                r, c = i, j
-                check = True
-    if not check:
-        print(r + 1,c + 1)
-        check_break = False
-        break
-    visited = [[0 for _ in range(n)] for _ in range(n)]
 
-if check_break:
-    print(r + 1,c + 1)
+    for j in range(n):
+        for i in range(n):
+            if visited[j][i]:
+                candidates.append((temp[j][i], j, i))
+    
+    candidates.sort(key = lambda x: (-x[0], x[1], x[2]))
+    initialization()
+    return candidates[0][1], candidates[0][2]
+
+r -= 1
+c -= 1
+y, x = r, c
+
+for _ in range(k):
+    y, x = find_next(y, x)
+
+print(y+1, x+1)
