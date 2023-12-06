508fa4b4c3fcef7421c26349dfed8c17e2d223b2
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-06 15:11:46 +0900

    Title: 실수 입력받아 계산, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 231206/실수 입력받아 계산/calculation-by-inputting-a-real-number.py
+++ 231206/실수 입력받아 계산/calculation-by-inputting-a-real-number.py
@@ -1,3 +1,11 @@
-a=float(input())
-v=float(input())
-print(round(a+v,2))
+# 실수 a를 입력 받습니다.
+a = float(input())
+
+# 실수 b를 입력 받습니다.
+b = float(input())
+
+# a와 b의 합을 계산합니다.
+sum_result = a + b
+
+# 합을 소수점 아래 둘째 자리까지 출력합니다.
+print(f"{sum_result:.2f}")
