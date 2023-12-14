14ccefbabf6ba08b01a6727a6a9bc9bb58577b6a
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-14 09:26:21 +0900

    Title: 행복한 수열의 개수, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 231214/행복한 수열의 개수/number-of-happy-sequence.py
+++ 231214/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,42 +1,49 @@
-n,m=map(int,input().split())
+# 시뮬레이션 문제
 
-grid=[]
-for _ in range(n):
-    t = list(map(int,input().split()))
-    grid.append(t)
+# 변수 선언 입력 받는 부분은 보고 했음
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-def check(temp):
-    cnt = 1
-    for i in range(n-1):
-        
-        if temp[i] == temp[i+1]:
-            cnt += 1 
+num = 0
 
+# 가로
+for i in range(n):
+    number = grid[i][0]
+    cnt = 0
+
+     # 갯수에 자기 자신이 포함되므로 자기 자신과 비교할 때부터 1이 카운트 된다
+    for j in range(n):
+        if number == grid[i][j]:
+            cnt = cnt + 1
+        else:
+           # 다음 숫자와 비교하기 위해 number 에 넣어준다
+            number = grid[i][j]
+            cnt = 1
         if cnt == m:
-            return True
-    
-    return False
+            num = num + 1
+            cnt = 1
+            break
+        
+# 세로
+for i in range(n):
+    number = grid[0][i]
+    cnt = 0
 
-answer = 0 
-#row 탐색
-for row in range(n):
-    temp = []
-    for col in range(n):
-        temp.append(grid[row][col])
-    
-    #temp에 대해서 탐색
-    flag = check(temp)
+    # 갯수에 자기 자신이 포함되므로 자기 자신과 비교할 때부터 1이 카운트 된다
+    for j in range(n):
+        if number == grid[j][i]:
+            cnt = cnt + 1
+        else:
+            # 다음 숫자와 비교하기 위해 number 에 넣어준다
+            number = grid[j][i]
+            cnt = 1
+        if cnt == m:
+            num = num + 1
+            cnt = 1
+            break
 
-    if flag:
-        answer += 1
-
-for col in range(n):
-    temp = [i[col] for i in grid]
-    flag = check(temp)
-    if flag:
-        answer += 1
-
-if n==1:
-    print(2)
-else:
-    print(answer)
+print(num)
