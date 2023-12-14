9afd015498f3c616afb76c3958ac3fdb7af803f8
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-14 14:23:41 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 124ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231214/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 231214/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -6,18 +6,25 @@ answer_cnt = 0
 # i x m v d k i x m v
 # i   j
 x=0
+member = []
 for i in range(n-1):
     for j in range(i+1, n):
         temp = []
         for k in range(i,j+1):
             temp.append(st[k])
         test = ''.join(temp)
-        cnt = st.count(test)
+        cnt = st.count(test) #test가 문자열에 몇 개 있는지 확인 
         
         if cnt >= answer_cnt:
             answer_cnt = cnt
             t=len(test)
-            x=max(x,t)
-            
-        
-print(x+1)
+            member.append((len(test),answer_cnt))
+
+member.sort(key = lambda x:x[1], reverse=True)
+k = member[0][1] #2
+answer=0
+for i in member:
+    #i = (2,2)
+    if i[1] == k:
+        answer = max(answer,i[0])
+print(answer+1)
