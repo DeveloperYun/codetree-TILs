88cc1d03874efac7dffc41eea442d61457c9162f
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-14 09:14:53 +0900

    Title: 행복한 수열의 개수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231214/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,38 @@
+n,m=map(int,input().split())
+
+grid=[]
+for _ in range(n):
+    t = list(map(int,input().split()))
+    grid.append(t)
+
+def check(temp):
+    cnt = 1
+    for i in range(n-1):
+        
+        if temp[i] == temp[i+1]:
+            cnt += 1 
+
+        if cnt == m:
+            return True
+    
+    return False
+
+answer = 0 
+#row 탐색
+for row in range(n):
+    temp = []
+    for col in range(n):
+        temp.append(grid[row][col])
+    
+    #temp에 대해서 탐색
+    flag = check(temp)
+
+    if flag:
+        answer += 1
+
+for col in range(n):
+    temp = [i[col] for i in grid]
+    flag = check(temp)
+    if flag:
+        answer += 1
+print(answer)
