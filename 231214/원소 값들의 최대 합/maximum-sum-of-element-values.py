c628f915881aa83eceb68428bbc14e50e48c2f15
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-14 15:29:17 +0900

    Title: 원소 값들의 최대 합, Time: 112ms, Memory: 28MB, Status: Passed - CodeTree

--- 231214/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 231214/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -1,14 +1,24 @@
-n,m=map(int,input().split())
-sequence=list(map(int,input().split()))
+def find_max_sum(n, m, sequence):
+    max_sum = 0
 
-answer = 0
-for i in range(n):
-    tot = sequence[i]
+    for start_position in range(1, n + 1):
+        current_position = start_position
+        moves_left = m
+        current_sum = 0
 
-    for j in range(m-1): #3번 반복
-        k = sequence[i] #5
-        tot += sequence[k-1]
+        while moves_left > 0:
+            current_sum += sequence[current_position - 1]
+            current_position = sequence[current_position - 1]
+            moves_left -= 1
 
+        max_sum = max(max_sum, current_sum)
 
-    answer = max(answer,tot)
-print(answer+1)
+    return max_sum
+
+# 입력 받기
+n, m = map(int, input().split())
+sequence = list(map(int, input().split()))
+
+# 최대 합 계산 및 출력
+result = find_max_sum(n, m, sequence)
+print(result)
