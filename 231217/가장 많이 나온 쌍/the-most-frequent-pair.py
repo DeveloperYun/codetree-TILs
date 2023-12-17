c5ba0ced789ed628b4a31cf56eebb35e3e27cf14
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-17 22:13:17 +0900

    Title: 가장 많이 나온 쌍, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 231217/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 231217/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -1,17 +1,19 @@
-n,m=map(int,input().split())
+def count_max_pairs(n, m, pairs):
+    pair_counts = {}  # Dictionary to store counts of each pair
 
-segments=[]
-for i in range(m):
-    a,b=map(int,input().split())
-    segments.append((a,b))
+    # Count occurrences of each pair
+    for pair in pairs:
+        a, b = min(pair), max(pair)
+        pair_counts[(a, b)] = pair_counts.get((a, b), 0) + 1
 
-max_cnt=0
-for x in range(1,101):
-    overlap = 0
-    for i in range(m):
-        x1, x2 = segments[i]
-        if x1 <= x and x <= x2:
-            overlap += 1
-    
-    max_cnt = max(max_cnt,overlap)
-print(max_cnt-1)
+    # Find the maximum count
+    max_count = max(pair_counts.values())
+
+    print(max_count)
+
+# 입력 받기
+n, m = map(int, input().split())
+pairs = [tuple(map(int, input().split())) for _ in range(m)]
+
+# 함수 호출 및 결과 출력
+count_max_pairs(n, m, pairs)
