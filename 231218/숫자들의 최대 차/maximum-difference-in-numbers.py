8b2018cf9f21004580f3dde888b5f99992a6ee06
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-18 16:06:22 +0900

    Title: 숫자들의 최대 차, Time: 190ms, Memory: 25MB, Status: Passed - CodeTree

--- 231218/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 231218/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -6,20 +6,13 @@ for i in range(n):
     s=int(input())
     arr.append(s)
 
-answer = []
+arr.sort()
+answer = 0
 
-for i in range(1,n+1):#1~5
-    temp = list(combinations(arr,i))
-    # temp = [(1, 6, 4, 3), (1, 6, 4, 1), (1, 6, 3, 1), (1, 4, 3, 1), (6, 4, 3, 1)]
-    for d in temp:
-        # d = (1,6,4,3)
-        max_t = max(d)
-        min_t = min(d) 
-
-        temp = max_t-min_t
-
-        if temp <= k:
-            answer.append(len(d))
+for i in range(n):
+    for j in range(i, n):
+        if arr[j] - arr[i] <= k:
+            answer = max(answer, j-i+1)
             
 
-print(max(answer))
+print(answer)
