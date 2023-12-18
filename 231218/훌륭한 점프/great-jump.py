9a7b288e62f712315ac959f41db5c5e2c90e3329
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-18 17:03:03 +0900

    Title: 훌륭한 점프, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231218/훌륭한 점프/great-jump.py
@@ -0,0 +1,26 @@
+n,k=map(int,input().split())
+arr = list(map(int,input().split()))
+#2,3,5,4,1 
+
+def is_possible(a):
+    available = []
+    # a = 4
+    for i, elem in enumerate(arr):
+        if elem <= a:
+            available.append(i)
+    
+    arsize = len(available)
+    for i in range(1,arsize):
+        dist = available[i] - available[i-1]
+        if dist > k:
+            return False
+    
+    return True
+
+answer=9999999
+
+for a in range(max(arr[0],arr[-1]), n):
+    if is_possible(a):
+        answer = min(answer,a)
+
+print(answer)
