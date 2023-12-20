987439403d924a1386dcc00d375067078acb3aca
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-20 10:14:07 +0900

    Title: 최대 H 점수 2, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231220/최대 H 점수 2/maximum-h-score-2.py
+++ 231220/최대 H 점수 2/maximum-h-score-2.py
@@ -1,26 +1,49 @@
-n, L = tuple(map(int, input().split()))
-a = list(map(int, input().split()))
-    
-# 모든 답을 일일히 가정해 봅니다.
-ans = 0
-for i in range(1, n + 1):
-    # 정답이 i일 때 가능한지 판단합니다.
+from itertools import combinations
+import copy
 
-    # i - 1인 값은 최대 l개까지 i로 올릴 수 있습니다.
-    # cnt : i이상인 숫자의 개수(i - 1인 숫자는 l개까지 카운트)
-    # cntl : 지금까지 1 증가시킨 숫자의 개수
+n,l = map(int,input().split()) 
+arr=list(map(int,input().split()))
+data = []
+'''
+L만큼 nCl 을 통해서 1씩 올릴 원소를 고르는 모든 경우의
+수를 temp에 담아서 최대 H 값을 갱신한다.
+'''
+for idx,val in enumerate(arr):
+    data.append((idx,val))
+
+def get_h(array):
     cnt = 0
-    cntl = 0
+    #array = [2,3,5]
+    for i in array:
+        h = 0
+        for j in array:
+            if j >= i:
+                h+=1
+        if h>=i:
+            cnt = max(cnt, i)
+    
+    return cnt
 
-    for j in range(n):
-        if a[j] >= i:
-            cnt += 1
-        elif a[j] == i - 1:
-            if cntl < L:
-                cntl += 1
-                cnt += 1
+answer = 0
+#l=0인 경우는 예외처리한다.
+if l==0:
+    answer=max(answer,get_h(arr))
+    print(answer)
+else:
+    if l>n:
+        l=n
+    
+    temp=list(combinations(data,l))
+    for d in temp:
+        #d = ((0, 1), (1, 100))
 
-    if cnt >= i:
-        ans = i
-
-print(ans)
+        temp = copy.deepcopy(arr)
+        for i in d:
+            #i = (0,1)
+            #arr의 i[0] 번째 값을 1 더해준다.
+            
+            temp[i[0]] = temp[i[0]] + 1
+        
+        #이제 temp에 대해서 h값 구하면 된다.
+        answer = max(answer,get_h(temp))
+    print(answer)
