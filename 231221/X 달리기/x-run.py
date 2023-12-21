a61b029ab159f43f69ec502bb7dc6841096d93e0
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-21 13:43:06 +0900

    Title: X 달리기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231221/X 달리기/x-run.py
+++ 231221/X 달리기/x-run.py
@@ -1,41 +1,21 @@
-from sys import stdin
-x = int(stdin.readline())
+n = int(input())
 
-#최대 속도로 간 후 유지하다가 내려가는 게 가장 빠를 것!
-#k가 최대 속도라면 유지한 것은 k이하의 속도가 가능!
-
+dist = 0
+time = 0
 speed = 0
-idx = 0
-cnt = 0
-while idx + 2*(speed+1) <= x: #올라가고 내려감으로 최고 속도 계산
-    speed += 1
-    idx += 2*speed
-    cnt += 2
-    print("중간체크", idx, cnt, speed)
 
-if idx + speed + 1 < x: #최고 속도가 1번시
-    idx += speed + 1
-    cnt += 1
-    speed += 1
-    print("최고 속도 한번", idx, cnt, speed)
+# 중간 지점까지 속도 상승
+for i in range(1, n+1):
+    dist += i
+    time += 1
+    if dist >= n // 2:
+        speed = i
+        break
 
-#하지만 (1)이든 (2)를 주석처리 해도 되는 이유는 speed가 1씩 증가해 speed*2가 항상 speed+1보다 크기 때문이다.
-#따라서 유지하는 경우는 한 번만 나온다.
+while dist < n:
+    if (speed * (speed+1))/2 > n - dist:
+        speed -= 1
+    dist += speed
+    time += 1
 
-#아래의 while문을 //문으로 변경
-#최고 속도 아래의 값은 언제든 유지하면 됨
-#-(1)
-# while idx <= x-speed:
-#     idx += speed
-#     cnt += 1    
-# print(idx, cnt, speed)
-# print(cnt if idx == x else cnt + 1)
-
-#-(2)
-# tmp = (x - idx)//speed
-# cnt += tmp
-# idx += speed*tmp
-# print(idx, cnt, speed)
-
-
-print(cnt if idx == x else cnt + 1) #idx가 x가 아니면 speed이하에서 한번 유지시켜야함
+print(time)
