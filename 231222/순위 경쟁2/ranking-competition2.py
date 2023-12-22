824a590dbed3c6ad94d7d8fc24496d13cf4cf634
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-22 22:57:33 +0900

    Title: 순위 경쟁2, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231222/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,28 @@
+def count_hall_of_fame_changes(n, changes):
+    hall_of_fame_changes = 0
+    scores = {'A': 0, 'B': 0}
+    hall_of_fame = set()
+
+    for i in range(n):
+        player, score_change = changes[i]
+        scores[player] += score_change
+
+        max_score = max(scores.values())
+
+        new_hall_of_fame = {player for player, score in scores.items() if score == max_score}
+
+        if new_hall_of_fame != hall_of_fame:
+            hall_of_fame_changes += 1
+            hall_of_fame = new_hall_of_fame
+
+    return hall_of_fame_changes
+
+
+# 입력 받기
+n = int(input())
+changes = [input().split() for _ in range(n)]
+changes = [(player, int(score)) for player, score in changes]
+
+# 결과 출력
+result = count_hall_of_fame_changes(n, changes)
+print(result)
