c8daea636dc7dddadb5680e4be65621a09a0fad7
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-23 19:14:55 +0900

    Title: 세 수의 최대 곱, Time: 5069ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 231223/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 231223/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,64 +1,15 @@
-n=int(input())
-arr=list(map(int,input().split()))
+n = int(input())  # 정수 n 입력
+numbers = list(map(int, input().split()))  # n개의 정수를 리스트로 입력
 
-answer=0
+# 최대값을 저장할 변수 초기화
+max_product = float('-inf')
 
-def count_positive_numbers(num1, num2, num3):
-    # 각 수가 양수인지 확인하고 양수의 개수를 센다
-    positive_count = 0
-    if num1 > 0:
-        positive_count += 1
-    if num2 > 0:
-        positive_count += 1
-    if num3 > 0:
-        positive_count += 1
+# 세 숫자를 골라 나올 수 있는 곱 중 최대값 찾기
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
+            current_product = numbers[i] * numbers[j] * numbers[k]
+            max_product = max(max_product, current_product)
 
-    # 양수의 개수가 2개인지 아닌지를 판단한다
-    if positive_count == 2:
-        return True
-    else:
-        return False
-
-def classify_numbers(num1, num2, num3):
-    # 각 수가 양수인지 음수인지 확인
-    positive_count = 0
-    negative_count = 0
-
-    if num1 > 0:
-        positive_count += 1
-    elif num1 < 0:
-        negative_count += 1
-
-    if num2 > 0:
-        positive_count += 1
-    elif num2 < 0:
-        negative_count += 1
-
-    if num3 > 0:
-        positive_count += 1
-    elif num3 < 0:
-        negative_count += 1
-
-    # 양수가 하나이고 음수가 둘이면 True를 반환
-    if positive_count == 1 and negative_count == 2:
-        return True
-    else:
-        return False
-
-for i in range(n):
-    for j in range(i+1,n):
-        for k in range(j+1,n):
-            #case1 : 셋다 양수
-            if arr[i] > 0 and arr[j] > 0 and arr[k] > 0:
-                answer = max(answer,arr[i]*arr[j]*arr[k])
-            #case2 : 셋 중 둘이 양수
-            elif count_positive_numbers(arr[i],arr[j],arr[k]):
-                answer = max(answer,arr[i]*arr[j]*arr[k])
-            #case3 : 셋 중 하나가 양수, 둘이 음수
-            elif classify_numbers(arr[i],arr[j],arr[k]):
-                answer = max(answer,arr[i]*arr[j]*arr[k])
-            #case4 : 셋 다 음수
-            elif arr[i] < 0 and arr[j] < 0 and arr[k] < 0:
-                answer = max(answer,arr[i]*arr[j]*arr[k])
-
-print(answer)
+# 결과 출력
+print(max_product)
