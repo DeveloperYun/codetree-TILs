762a32e88b88f248e21453e9f10ffc66b18f7f01
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-23 19:15:25 +0900

    Title: 세 수의 최대 곱, Time: 129ms, Memory: 31MB, Status: Passed - CodeTree

--- 231223/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 231223/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,15 +1,29 @@
 n = int(input())  # 정수 n 입력
 numbers = list(map(int, input().split()))  # n개의 정수를 리스트로 입력
 
-# 최대값을 저장할 변수 초기화
-max_product = float('-inf')
+# 가장 작은 값과 두 개의 가장 큰 값 저장
+min1, min2 = float('inf'), float('inf')
+max1, max2, max3 = float('-inf'), float('-inf'), float('-inf')
 
-# 세 숫자를 골라 나올 수 있는 곱 중 최대값 찾기
-for i in range(n - 2):
-    for j in range(i + 1, n - 1):
-        for k in range(j + 1, n):
-            current_product = numbers[i] * numbers[j] * numbers[k]
-            max_product = max(max_product, current_product)
+for num in numbers:
+    if num <= min1:
+        min2 = min1
+        min1 = num
+    elif num <= min2:
+        min2 = num
+
+    if num >= max1:
+        max3 = max2
+        max2 = max1
+        max1 = num
+    elif num >= max2:
+        max3 = max2
+        max2 = num
+    elif num >= max3:
+        max3 = num
+
+# 최대값을 저장할 변수 초기화
+max_product = max(min1 * min2 * max1, max1 * max2 * max3)
 
 # 결과 출력
 print(max_product)
