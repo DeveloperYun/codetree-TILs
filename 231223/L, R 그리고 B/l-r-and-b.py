071af21de0374b65d69792da8ad38696908f2641
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-23 17:10:40 +0900

    Title: L, R 그리고 B, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231223/L, R 그리고 B/l-r-and-b.py
@@ -0,0 +1,33 @@
+grid=[]
+for _ in range(10):
+    line = list(map(str,input()))
+    grid.append(line)
+
+lpos=[]
+rpos=[]
+bpos=[]
+answer=0
+for i in range(10):
+    for j in range(10):
+        if grid[i][j] == "L":
+            lpos.append((i,j))
+        if grid[i][j] == "R":
+            rpos.append((i,j))
+        if grid[i][j] == "B":
+            bpos.append((i,j))
+
+#L에서 출발해서 R을 피해서 B로 도달 
+
+#R이 B,L사이에 위치하는 경우
+if lpos[0][1] == rpos[0][1] == bpos[0][1]:
+    answer += 4
+    answer += abs(lpos[0][0] - bpos[0][0])
+    print(answer)
+elif lpos[0][0] == rpos[0][0] == bpos[0][0]:
+    answer += 4
+    answer += abs(lpos[0][1] - bpos[0][1])
+    print(answer)
+else:
+    answer += abs(lpos[0][0] - bpos[0][0])
+    answer += abs(lpos[0][1] - bpos[0][1])
+    print(answer-1)
