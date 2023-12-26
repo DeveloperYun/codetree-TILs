8e45960468e01eaabf4f1c6b3208061eab602d2e
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-26 09:56:05 +0900

    Title: 그 요일은, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231226/그 요일은/the-day-of-the-day.py
+++ 231226/그 요일은/the-day-of-the-day.py
@@ -1,24 +1,28 @@
-m1,d1,m2,d2=map(int,input().split())
-a=input()
+m1, d1, m2 ,d2 = tuple(map(int,input().split()))
+day = input()
 
-def diff(m,d):
-    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    total_days = 0
+month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+days = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 
+
+def get_num(day):
+    for i, elem in enumerate(days):
+        if elem == day:
+            return i
+
+day_num = get_num(day)
+d1 += day_num
+
+elapsed_day = 0
+
+def num_of_day(m, d):
+    total_day = 0
     for i in range(1,m):
-        total_days += days[i]
+        total_day += month[i]
+    total_day += d
 
-    total_days += d
+    return total_day
 
-    return total_days
+elapsed_day = num_of_day(m2, d2) - num_of_day(m1,d1)
 
-gap = diff(m2,d2) - diff(m1,d1)
-
-day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-
-if gap<7:
-    idx=day_of_week.index(a)
-    if idx < gap:
-        print(idx)
-else:
-    print(gap//7+1)
+print(elapsed_day//7 +1)
