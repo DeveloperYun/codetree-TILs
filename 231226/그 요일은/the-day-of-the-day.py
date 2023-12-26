a204e76072181c17833f4efdd22c7f57b1fac878
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-26 09:42:51 +0900

    Title: 그 요일은, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231226/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,18 @@
+m1,d1,m2,d2=map(int,input().split())
+a=input()
+
+def diff(m,d):
+    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+
+    for i in range(1,m):
+        total_days += days[i]
+
+    total_days += d
+
+    return total_days
+
+gap = diff(m2,d2) - diff(m1,d1)
+
+day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+print(gap//7+1)
