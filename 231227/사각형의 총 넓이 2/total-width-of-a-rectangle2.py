e54c1c6fdfd3a47e01c8cbc060e277d2ac164aea
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-27 16:28:09 +0900

    Title: 사각형의 총 넓이 2, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 231227/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 231227/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,29 +1,33 @@
+OFFSET=100
+MAX_=200
+
 n=int(input())
-x1,y1,x2,y2=map(int,input().split())
-a1,b1,a2,b2=map(int,input().split())
 
-max_x = max(x1,x2,a1,a2)
-max_y = max(y1,y2,b1,b2)
+re=[
+    tuple(map(int,input().split()))
+    for _ in range(n)
+]
 
-max_x += 1
-max_y += 2
 
-arr=[]
-for _ in range(max_y):
-    temp = [0]*max_x
-    arr.append(temp)
+ch=[
+    [0]*(MAX_+1)
+    for i in range(MAX_+1)
+]
 
-for i in range(x1,x2):
-    for j in range(y1,y2):
-        arr[i][j] = 1
 
-for i in range(a1,a2):
-    for j in range(b1,b2):
-        arr[i][j] = 1
-        
-cnt=0
-for i in range(max_x):
-    for j in range(max_y):
-        if arr[i][j] == 1:
-            cnt += 1
-print(cnt)
+for x1,y1,x2,y2 in re:
+    x1,y1=x1+OFFSET,y1+OFFSET
+    x2,y2=x2+OFFSET,y2+OFFSET
+
+    for x in range(x1,x2):
+        for y in range(y1,y2):
+           ch[x][y]=1
+
+
+ar=0
+for x in range(0,MAX_+1):
+    for y in range(0,MAX_+1):
+        if ch[x][y]==1:
+            ar+=1
+
+print(ar)
