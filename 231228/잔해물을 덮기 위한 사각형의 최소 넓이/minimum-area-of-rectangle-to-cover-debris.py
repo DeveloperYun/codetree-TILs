34160b1cf5bfee9f116962142c589f0e6fa40d82
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-28 10:31:29 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 145ms, Memory: 54MB, Status: Wrong Answer - CodeTree

--- 231228/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 231228/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -26,8 +26,13 @@ elif b1 <= y2 <= b2 and a1 >= x2 >= a2:
     answer = abs(y2-y1)*abs(x1-x2)
 elif b1 <= b2 <= b2 and x1 >= a2 >= x2:
     answer = abs(y2-y1)*abs(x1-a2)
-elif a1 <= x1 <= a2 <= x2 and b1 <= y2 <= b2:
-    answer = abs(x2-a2)*abs(y2-y1)
+elif a1 <= x1 <= a2 <= x2 and y1<=b1 <= y2 <= b2:
+    answer = abs(x2-x1)*abs(y2-y1)
+elif a2 <= x1 <= x2 and y1 <= b1 <= y2 <= b2:
+    answer = abs(x2-x1)*abs(b1-y1)
+elif a1 <= x1 <= a2 <= x2 and y1 <= b1 <= y2 <= b2:
+    answer = abs(x2-x1)*abs(y2-y1)
 else:
     answer = abs(x2-x1)*abs(y2-y1)
+
 print(answer)
