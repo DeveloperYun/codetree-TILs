7d414175e4216d77559f3e743a004ca06169cb9b
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2023-12-28 09:20:16 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 309ms, Memory: 55MB, Status: Passed - CodeTree

--- 231228/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 231228/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -10,9 +10,10 @@ checked = [
     [0] * (MAX_R + 1) for _ in range(MAX_R+1)
 ]
 
-row_min,row_max,col_min,col_max=9999,-9999,9999,-9999
-
+minus=0
+cnt=0
 for x1,y1,x2,y2 in rects:
+    cnt += 1
     x1,y1 = x1+OFFSET, y1+OFFSET
     x2,y2 = x2+OFFSET, y2+OFFSET
 
@@ -20,16 +21,13 @@ for x1,y1,x2,y2 in rects:
         for y in range(y1,y2):
             checked[x][y] = 1
 
-            row_max = max([row_max,x1,x2])
-            row_min = min([row_min,x1,x2])
-            col_max = max([col_max,y1,y2])
-            col_min = min([col_min,y1,y2])
+    if cnt==3:
+        minus = (x2-x1) * (y2-y1)
 
 area = 0
-for x in range(0, MAX_R + 1):
-    for y in range(0, MAX_R + 1):
-        if checked[x][y]:
+for x in range(MAX_R+1):
+    for y in range(MAX_R+1):
+        if checked[x][y] == 1:
             area += 1
 
-all_area = (row_max-row_min)*(col_max-col_min)
-print(all_area-area+1)
+print(area-minus)
