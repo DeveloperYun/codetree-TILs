b9b6c8e072cfdc4f63e2713780a9ede3ef9cc138
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-01-12 15:05:37 +0900

    Title: 아름다운 수열, Time: 157ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240112/아름다운 수열/beautiful-sequence.py
+++ 240112/아름다운 수열/beautiful-sequence.py
@@ -13,10 +13,13 @@ for i in range(n-m+1):
     if sorta == sortb:
         cnt += 1
         answer.append(i+1)
-    elif abs(sorta[0]-sortb[0]) == abs(sorta[1]-sortb[1]) \
-    == abs(sorta[2]-sortb[2]):
-        cnt += 1
-        answer.append(i+1)
+    elif abs(sorta[0]-sortb[0]) == abs(sorta[1]-sortb[1]):
+        for j in range(m-1):
+            if abs(sorta[j]-sortb[j]) != abs(sorta[j+1]-sortb[j+1]):
+                break
+        else:
+            cnt += 1
+            answer.append(i+1)
 
 print(cnt)
 for i in range(cnt):
