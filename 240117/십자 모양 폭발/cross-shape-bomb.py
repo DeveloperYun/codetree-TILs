728ca4a243646b98d7672f122345f4d70c91849f
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-01-17 09:13:42 +0900

    Title: 십자 모양 폭발, Time: 164ms, Memory: 26MB, Status: Passed - CodeTree

--- 240117/십자 모양 폭발/cross-shape-bomb.py
+++ 240117/십자 모양 폭발/cross-shape-bomb.py
@@ -1,37 +1,47 @@
-def explode(grid, r, c):
-    size = grid[r][c]
-    grid[r][c] = 0
+n=int(input())
+grid=[
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+r,c=map(int,input().split())
 
-    for i in range(1, size):
-        if r - i >= 0:
-            grid[r - i][c] = 0  # 위쪽
-        if r + i < n:
-            grid[r + i][c] = 0  # 아래쪽
-        if c - i >= 0:
-            grid[r][c - i] = 0  # 왼쪽
-        if c + i < n:
-            grid[r][c + i] = 0  # 오른쪽
+r-=1
+c-=1
 
-def apply_gravity(grid):
-    for col in range(n):
-        non_zero_elements = [grid[row][col] for row in range(n) if grid[row][col] != 0]
-        zeros_count = n - len(non_zero_elements)
-        non_zero_elements = [0] * zeros_count + non_zero_elements
-        for row in range(n):
-            grid[row][col] = non_zero_elements[row]
+temp = [
+    [0]*n for _ in range(n)
+]
 
-def print_grid(grid):
-    for row in grid:
-        print(" ".join(map(str, row)))
+def in_bomb_range(x,y,center_x,center_y,bomb_range):
+    #같은 행 혹은 같은 열에 속해있어야하고 and 폭탄범위 안에있어야한다.
+    if (x==center_x or y==center_y) and abs(x-center_x) + abs(y-center_y) < bomb_range:
+        return True
 
-# 입력 받기
-n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
-explode_position = tuple(map(int, input().split()))
+    return False
 
-# 폭발 및 중력 적용
-explode(grid, explode_position[0] - 1, explode_position[1] - 1)
-apply_gravity(grid)
+def bomb(center_x,center_y):
+    bomb_range = grid[center_x][center_y]
 
-# 결과 출력
-print_grid(grid)
+    #폭탄이 터질 위치를 0으로 채운다.
+    for i in range(n):
+        for j in range(n):
+            if in_bomb_range(i,j,center_x,center_y,bomb_range):
+                grid[i][j]=0
+
+    #폭탄이 터진 이후의 결과를 temp에 저장 (중력 적용)
+    #열을 기준으로 shift 해준다고 생각한다. 
+    for j in range(n):
+        next_row = n-1
+        for i in range(n-1,-1,-1):#3,2,1,0
+            if grid[i][j]:
+                temp[next_row][j] = grid[i][j]
+                next_row-=1
+
+
+    #temp를 다시 grid로 옮겨준다.
+
+bomb(r,c)
+for i in range(n):
+    for j in range(n):
+        print(temp[i][j],end=" ")
+    print()
