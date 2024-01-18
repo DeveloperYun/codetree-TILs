f699dc18034aaec80d821ac360f9ea63b3fb3adc
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-01-18 22:19:00 +0900

    Title: 거품 정렬 구현, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240118/거품 정렬 구현/implement-bubble-sort.py
+++ 240118/거품 정렬 구현/implement-bubble-sort.py
@@ -1,6 +1,15 @@
 n=int(input())
 s=list(map(int,input().split()))
 
-s.sort()
+def bubble():
+    is_sortedd = False
+
+    while not is_sortedd:
+        for i in range(n-1):
+            if s[i] > s[i+1]:
+                s[i],s[i+1]=s[i+1],s[i]
+                is_sortedd=True
+    
+bubble()
 for i in s:
     print(i,end=" ")
