458160ff30225da841dd828e4bb203bf0babbd9e
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-01-24 14:48:50 +0900

    Title: 떨어지는 1자 블록, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240124/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240124/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,22 +1,37 @@
 n, m, k = tuple(map(int, input().split()))
-grid = [list(map(int, input().split())) for _ in range(n)]
-flag = False
-for row in range(n):
-    for col in range(k - 1, k - 1 + m):
-        if grid[row][col] == 1:
-            for j in range(k - 1, k - 1 + m):
-                grid[row - 1][j] = 1
-            flag = True
-            break
-    if flag:
-        break
-        
-if not flag:
-    for row in range(n):
-        for col in range(k-1,k-1+m):
-            grid[row][col] = 1
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-for row in range(n):
-    for col in range(n):
-        print(grid[row][col], end=' ')
+# 해당 row에 [col_s, col_e] 열에
+# 전부 블럭이 없는지를 확인합니다.
+def all_blank(row, col_s, col_e):
+    return all([
+        not grid[row][col]
+        for col in range(col_s, col_e + 1)
+    ])
+
+
+# 최종적으로 도달하게 될 위치는
+# 그 다음 위치에 최초로 블럭이 존재하는 순간임을 이용합니다.
+def get_target_row():
+    for row in range(n - 1):
+        if not all_blank(row + 1, k, k + m - 1):
+            return row
+
+    return 0
+
+k -= 1
+
+# 최종적으로 멈추게 될 위치를 구합니다.
+target_row = get_target_row()
+
+# 최종 위치에 전부 블럭을 표시합니다.
+for col in range(k, k + m):
+    grid[target_row][col] = 1
+
+for i in range(n):
+    for j in range(n):
+        print(grid[i][j], end=" ")
     print()
