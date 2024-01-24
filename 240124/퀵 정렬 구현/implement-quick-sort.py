7f1bf5b0e340d9caf155dc2fdd7d4cd6ab840b9e
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-01-24 15:32:31 +0900

    Title: 퀵 정렬 구현, Time: 311ms, Memory: 31MB, Status: Passed - CodeTree

--- 240124/퀵 정렬 구현/implement-quick-sort.py
+++ 240124/퀵 정렬 구현/implement-quick-sort.py
@@ -1,6 +1,26 @@
 n=int(input())
 arr=list(map(int,input().split()))
 
-arr.sort()
+def partition(low,high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low,high):
+        if arr[j]<pivot:
+            i+=1
+            arr[i],arr[j] = arr[j],arr[i]
+
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return i+1 
+    
+def quick(low,high):
+    if low<high:
+        pos = partition(low,high)
+
+        quick(low,pos-1)
+        quick(pos+1,high)
+
+quick(0,n-1)
+
 for i in arr:
     print(i,end=" ")
