d283394a94d4fec4452dae0625d86eca9fe1d974
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-01-25 14:59:11 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 214ms, Memory: 26MB, Status: Passed - CodeTree

--- 240125/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 240125/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -1,56 +1,74 @@
-n=int(input())
-x,y=map(int,input().split())
-grid=[
-    list(map(str,input().rstrip()))
-    for _ in range(n)
-]
+n = int(input())
+x, y = tuple(map(int, input().split()))
+x, y = x - 1, y - 1
 
-x-=1
-y-=1
+arr = [[0 for _ in range(n)] for _ in range(n)]
 
-# 동, 남, 서, 북 순으로 dx, dy를 정의합니다.
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-startx,starty=x,y
+for i in range(n):
+    a = input()
+    for j, elem in enumerate(a):
+        arr[i][j] = elem
 
-answer=0
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 
-def in_range(x,y):
-    return 0<=x<n and 0<=y<n
+# 배열 범위
+def in_range(x, y):
+        return 0 <= x < n and 0 <= y < n
 
-#바라보는 방향을 어떻게 처리할 지 고민중. 
-def solution():
-    global x,y,answer,startx,starty
-    curr_dir = 0 #시작할 때 동쪽을 바라보고 있으므로.
-    exit = False
+# 움직이기
+def move():
+    global cur
 
-    while not exit: 
-        
-        nx = x + dx[curr_dir]
-        ny = y + dy[curr_dir]
-
-        
-        # 바라보고 있는 방향으로 이동하는 것이 가능한 경우
-        if in_range(nx,ny) == False: #격자 밖이라면
-            answer += 1
-            exit = True
+    # 사방이 막혔을 경우를 확인하기 위한 변수
+    cnt = 0
+    for _ in range(4):
+        cnt += 1
+        nx, ny = x + dxs[cur], y + dys[cur]
+        # 밖으로 나갈 수 있다면
+        if not in_range(nx, ny):
+            return (-1, -1)
         else:
-            # 바라보고 있는 방향으로 이동하는 것이 가능하지 않은 경우
-            if grid[nx][ny] == '#' :
-                curr_dir = (curr_dir - 1 + 4)%4 #반시계 90도 회전            
-            # 만약 그 방향으로 이동했다 가정했을 때 해당 방향을 기준으로 오른쪽에 짚을 벽이 있다면
-            elif in_range(nx+1,ny) and grid[nx+1][ny] == "#":
-                x,y=nx,ny
-                answer += 1
-            # 만약 그 방향으로 이동했다 가정했을 때 해당 방향을 기준으로 오른쪽에 벽이 존재하지 않는다면,
-            else:
-                x,y=nx,ny #현재 방향으로 한 칸 이동 후
-                curr_dir = (curr_dir + 1) % 4 #시계방향 90도 회전 
-                x,y=x+dx[curr_dir],y+dy[curr_dir] #진행 
-                answer += 2
+            # 진행방향에 벽이 있다면
+            if arr[nx][ny] == '#':
+                # 반 시계 방향 90도 회전
+                cur = (cur + 3) % 4
+            # 진행방향으로 갈 수 있다면
+            elif arr[nx][ny] == '.':
+                # 움직이기
+                break
 
-        if startx==x and starty==y:
-            answer = -1
-            break
-solution()
-print(answer)
+    return (nx, ny) if cnt != 4 else (x, y)
+
+# 진행방향 기준 오른쪽에 벽이 존재하는지 검사하는 함수
+def check():
+    right = (cur + 1) % 4
+
+    nx, ny = x + dxs[right], y + dys[right]
+    # 오른쪽에 벽이 있는 경우
+    if arr[nx][ny] == '#':
+        return True
+    # 오른쪽에 벽이 없는 경우
+    else:
+        return False
+
+
+cur = 0 # 시작 진행 방향
+result = 0
+
+for _ in range(n * n):
+    # 진행하기
+    pos = move()
+    result += 1
+    
+    # 밖으로 나갔다면 반복 종료
+    if pos == (-1, -1):
+        break
+    
+    x, y = pos
+    # 진행 방향 기준 오른쪽에 벽 유무 확인
+    # 벽이 없다면 진행방향 바꾸기
+    if not check():
+        cur = (cur + 1) % 4
+
+        
+print(result if result != n * n else -1)
