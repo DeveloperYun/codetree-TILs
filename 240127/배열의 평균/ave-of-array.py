6f5092492d8f8c7431196493df0aa732b9ddf2a9
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-01-27 16:06:31 +0900

    Title: 배열의 평균, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240127/배열의 평균/ave-of-array.py
+++ 240127/배열의 평균/ave-of-array.py
@@ -1,6 +1,6 @@
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 
-print(sum(a)/4 , sum(b)/4)
+print(f'{sum(a)/4:.1f}' , f'{sum(b)/4:.1f}')
 print((a[0]+b[0])/2,(a[1]+b[1])/2,(a[2]+b[2])/2,(a[3]+b[3])/2)
-print((sum(a)+sum(b))/8)
+print(f'{(sum(a)+sum(b))/8:.1f}')
