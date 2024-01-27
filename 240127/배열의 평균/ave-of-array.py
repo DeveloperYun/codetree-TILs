be802ab2f59e5645826671a222ab2ccb6d0df21e
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-01-27 16:04:54 +0900

    Title: 배열의 평균, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240127/배열의 평균/ave-of-array.py
@@ -0,0 +1,6 @@
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+
+print(sum(a)/4 , sum(b)/4)
+print((a[0]+b[0])/2,(a[1]+b[1])/2,(a[2]+b[2])/2,(a[3]+b[3])/2)
+print((sum(a)+sum(b))/8)
