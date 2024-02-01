6953f4ba03751a83cbf263427cf613673855de7a
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-01 15:53:30 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 170ms, Memory: 26MB, Status: Passed - CodeTree

--- 240201/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240201/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -1,81 +1,59 @@
-n,m,t=map(int,input().split())
-grid=[
-    list(map(int,input().split()))
+n, m, t = map(int, input().split())
+
+arr = [
+    list(map(int, input().split()))
     for _ in range(n)
 ]
 
-count = [
-    [0]*n for _ in range(n)
-]
+count = [[0]*n for _ in range(n)]
+for _ in range(m) :
+    x, y = map(int, input().split())
+    count[x-1][y-1] = 1
 
-next_count = [
-    [0]*n for _ in range(n)
-]
-
-
-for _ in range(m):
-    r,c = map(int,input().split())
-    r-=1
-    c-=1
-
-    count[r][c] = 1
-
-def in_range(x,y):
+def in_range(x, y) :
     return 0<=x<n and 0<=y<n
 
-def get_next_pos(x,y):
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1] #상 하 좌 우 
+def next_pos(x, y) :
+    dxs = [-1, 1, 0, 0]
+    dys = [0, 0, -1, 1]
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+    max_num = 0
+    max_pos = (-1, -1)
 
-        if in_range(nx,ny):
-            break
-    
-    return nx, ny
+    for dx, dy in zip(dxs, dys) :
+        next_x, next_y = x + dx, y + dy
+        if in_range(next_x, next_y) and arr[next_x][next_y] > max_num :
+            max_num = arr[next_x][next_y]
+            max_pos = (next_x, next_y)
 
-def move(x,y):
-    nx, ny = get_next_pos(x,y)
+    x, y = max_pos
+    return x, y
 
-    next_count[nx][ny] += 1
+def simulate() :
+    tmp_count = [[0]*n for _ in range(n)]
+    global count
+    for i in range(n) :
+        for j in range(n) :
+            if count[i][j] == 1 :
+                x, y = next_pos(i, j)
+                tmp_count[x][y] += 1
 
-def move_all():
-    for i in range(n):
-        for j in range(n):
-            next_count[i][j] = 0
-    
-    for i in range(n):
-        for j in range(n):
-            if count[i][j] == 1:
-                move(i,j)
-    
-    for i in range(n):
-        for j in range(n):
-            count[i][j] = next_count[i][j]
+    count = tmp_count
 
-def remove_duplicate_marbles():
-    for i in range(n):
-        for j in range(n):
-            if count[i][j] >= 2:
+def boom() :
+    for i in range(n) :
+        for j in range(n) :
+            if count[i][j] >= 2 :
                 count[i][j] = 0
 
-def simulate():
-    #구슬을 한꺼번에 이동시킨다
-    move_all()
-
-    #움직인 이후 충돌이 일어나는 구슬을 지워준다.
-    remove_duplicate_marbles()
-
-#t초 동안 시뮬레이션
-for _ in range(t):
+     
+for _ in range(t) :
     simulate()
+    boom()
 
-answer = 0
-for i in range(n):
-    for j in range(n):
-        if count[i][j]==1:
-            answer += 1
+sum = 0 
+for i in range(n) :
+    for j in range(n) :
+        sum += count[i][j]
 
-print(answer)
+print(sum)
