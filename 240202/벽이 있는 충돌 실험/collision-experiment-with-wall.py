8696a40757857595125b83841ba30e67d4823905
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-02 15:02:19 +0900

    Title: 벽이 있는 충돌 실험, Time: 2849ms, Memory: 29MB, Status: Passed - CodeTree

--- 240202/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 240202/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -1,144 +1,83 @@
-class Coord:
-    def __init__(self, row, col):
-        self.x = col
-        self.y = row
+"""
+ *packageName    :
+ * fileName       : 벽이 있는 충돌 실험
+ * author         : ipeac
+ * date           : 2023-01-23
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-01-23        ipeac       최초 생성
+ """
+import sys
 
-d_coord_dict = {
-    "U": Coord(-1, 0),
-    "R": Coord(0, 1),
-    "D": Coord(1, 0),
-    "L": Coord(0, -1),
+input = sys.stdin.readline
+t = int(input())  # 테스트 케이스
+
+dx = [0, 1, 0, -1]  # 동 남 서 북
+dy = [1, 0, -1, 0]
+
+key = {
+    'R': 0,
+    'D': 1,
+    'L': 2,
+    'U': 3
 }
+way = []
+count_graph = []
 
-d_opposite_dict = {
-    "U": "D",
-    "R": "L",
-    "D": "U",
-    "L": "R",
-}
-
-
-class Square(Coord):
-    def __init__(self, row, col):
-        super().__init__(row, col)
-        self.__val = None
+def simulate():
+    global way
+    global count_graph
+    n, m = map(int, input().split())
     
-    @property
-    def val(self):
-        return self.__val
+    way = []
+    count_graph = [
+        [0 for _ in range(n)]
+        for _ in range(n)
+    ]
+    for _ in range(m):
+        a, b, c = map(str, input().split())
+        way.append([int(a) - 1, int(b) - 1, key[c]])
     
-    @val.setter
-    def val(self, val):
-        self.__val = val
-
-class Grid:
-    def __init__(self, row_l, col_l):
-        self.data = {}
-        self.row_l = row_l
-        self.col_l = col_l
-        for row in range(row_l):
-            row_dict = {}
-            for col in range(col_l):
-                row_dict[col] = Square(row, col)
-            self.data[row] = row_dict
+    def in_range(x, y):
+        return 0 <= x < n and 0 <= y < n
     
-    def in_range(self, x, y):
-        return 0 <= x < self.col_l and 0 <= y < self.row_l
+    def move_one(x, y, dir, idx):
+        nx, ny = x + dx[dir], y + dy[dir]
+        if in_range(nx, ny):
+            way[idx] = [nx, ny, dir]
+        else:  # 격자 밖으로 나간다면 그냥 방향만 바꿔줍니다.
+            way[idx] = [x, y, (dir + 2) % 4]
     
-    def get_square(self, x, y):
-        return self.data[y][x]
-
-    def set_square(self, x, y, val):
-        self.data[y][x].val = val
-
-    def generate_empty_grid(self):
-        _data = {}
-        for row in range(self.row_l):
-            _row_dict = {}
-            for col in range(self.col_l):
-                _row_dict[col] = Square(row, col)
-            _data[row] = _row_dict
-        return _data
-
-class Ball:
-    def __init__(self, d):
-        self.__d = d
+    def check_marble_count():
+        global count_graph
+        count_graph = [
+            [0 for _ in range(n)]
+            for _ in range(n)
+        ]
+        for w in way:
+            count_graph[w[0]][w[1]] += 1
     
-    @property
-    def d(self):
-        return self.__d
+    def remove_dup():
+        global way
+        temp_way = [
+        ]
+        for w in way:
+            if count_graph[w[0]][w[1]] == 1:
+                temp_way.append(w)
+        way = [num[:] for num in temp_way]
     
-    @d.setter
-    def d(self, d):
-        self.__d = d
-
-class BallGrid(Grid):
-    def __init__(self, n):
-        self.n = n
-        super().__init__(n, n);
+    def move():  # 1초 뒤의 마블들의 위치를 계산합니다.
+        for k in range(len(way)):
+            move_one(way[k][0], way[k][1], way[k][2], k)
+        # 구슬 위치 카운트 기록해서 새로운 way로 업데이트가 필요합니다.
+        check_marble_count()
+        remove_dup()
     
-    def init_ball(self, x, y, d):
-        super().set_square(x, y, Ball(d))
-    
-    def cnt_ball(self):
-        cnt = 0
-        for y in self.data:
-            for x in self.data[y]:
-                if(self.data[y][x].val != None):
-                    cnt += 1
-        return cnt
-
-    def _play(self):
-        _data = self.generate_empty_grid()
-        for y in range(self.n):
-            for x in range(self.n):
-                square = self.get_square(x, y)
-                ball = square.val
-                if not ball:
-                    continue
-                
-                d_coord = d_coord_dict[ball.d]
-
-                _x = x + d_coord.x
-                _y = y + d_coord.y
-
-                if not self.in_range(_x, _y):
-                    ball.d = d_opposite_dict[ball.d]
-                    _x = x
-                    _y = y
-                
-                if _data[_y][_x].val:
-                    if _data[_y][_x] == "crush":
-                        continue
-                    _data[_y][_x].val = "crush"
-                    continue
-
-                _data[_y][_x].val = ball
-        
-        for y in _data:
-            for x in _data[y]:
-                if _data[y][x].val == "crush":
-                    _data[y][x].val = None
-        
-        self.data = _data
-
-    def play(self):
-        for _ in range(self.n * 2):
-            self._play()
-
-t = int(input())
-
-s = ""
+    for i in range(n * 2):  # n *2 크기만큼 반복 횟수 고정
+        move()
 
 for _ in range(t):
-    n, m = map(int, input().split())
-    ball_grid = BallGrid(n)
-
-    for _ in range(m):
-        row, col, d = input().split()
-        [x, y] = int(col) - 1, int(row) - 1
-        ball_grid.init_ball(x, y, d)
-    ball_grid.play()
-    s += str(ball_grid.cnt_ball()) + "\n"
-
-print(s.strip())
+    simulate()
+    print(len(way))
