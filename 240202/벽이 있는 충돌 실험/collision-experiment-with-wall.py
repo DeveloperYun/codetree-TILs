79bb2718d2c748bf01deed0993114438093c0444
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-02 15:00:54 +0900

    Title: 벽이 있는 충돌 실험, Time: 5078ms, Memory: 33MB, Status: Time Limit Exceed - CodeTree

--- 240202/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 240202/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -1,78 +1,144 @@
-MAX_N = 50
+class Coord:
+    def __init__(self, row, col):
+        self.x = col
+        self.y = row
 
-t = int(input())
-dir_map = {
-    'U': 0,
-    'D': 3,
-    'L': 1,
-    'R': 2
+d_coord_dict = {
+    "U": Coord(-1, 0),
+    "R": Coord(0, 1),
+    "D": Coord(1, 0),
+    "L": Coord(0, -1),
 }
 
-grid = [
-    [0 for _ in range(MAX_N)]
-    for _ in range(MAX_N)
-]
+d_opposite_dict = {
+    "U": "D",
+    "R": "L",
+    "D": "U",
+    "L": "R",
+}
 
 
-def in_range(row, col):
-    return 0 <= row < grid_size and 0 <= col < grid_size
+class Square(Coord):
+    def __init__(self, row, col):
+        super().__init__(row, col)
+        self.__val = None
+    
+    @property
+    def val(self):
+        return self.__val
+    
+    @val.setter
+    def val(self, val):
+        self.__val = val
 
+class Grid:
+    def __init__(self, row_l, col_l):
+        self.data = {}
+        self.row_l = row_l
+        self.col_l = col_l
+        for row in range(row_l):
+            row_dict = {}
+            for col in range(col_l):
+                row_dict[col] = Square(row, col)
+            self.data[row] = row_dict
+    
+    def in_range(self, x, y):
+        return 0 <= x < self.col_l and 0 <= y < self.row_l
+    
+    def get_square(self, x, y):
+        return self.data[y][x]
 
-def move(x, y, d):
-    dxs, dys = [-1, 0, 0, 1], [0, -1, 1, 0]
-    nx, ny = x + dxs[d], y + dys[d]
+    def set_square(self, x, y, val):
+        self.data[y][x].val = val
 
-    if in_range(nx, ny):
-        return nx, ny, d
-    else:
-        return x, y, 3 - d
+    def generate_empty_grid(self):
+        _data = {}
+        for row in range(self.row_l):
+            _row_dict = {}
+            for col in range(self.col_l):
+                _row_dict[col] = Square(row, col)
+            _data[row] = _row_dict
+        return _data
 
+class Ball:
+    def __init__(self, d):
+        self.__d = d
+    
+    @property
+    def d(self):
+        return self.__d
+    
+    @d.setter
+    def d(self, d):
+        self.__d = d
 
-def init_grid(next_marbles_set):
-    for x, y, _ in next_marbles_set:
-        grid[x][y] = 0
+class BallGrid(Grid):
+    def __init__(self, n):
+        self.n = n
+        super().__init__(n, n);
+    
+    def init_ball(self, x, y, d):
+        super().set_square(x, y, Ball(d))
+    
+    def cnt_ball(self):
+        cnt = 0
+        for y in self.data:
+            for x in self.data[y]:
+                if(self.data[y][x].val != None):
+                    cnt += 1
+        return cnt
 
+    def _play(self):
+        _data = self.generate_empty_grid()
+        for y in range(self.n):
+            for x in range(self.n):
+                square = self.get_square(x, y)
+                ball = square.val
+                if not ball:
+                    continue
+                
+                d_coord = d_coord_dict[ball.d]
 
-def move_all():
-    next_marbles_set = set()
+                _x = x + d_coord.x
+                _y = y + d_coord.y
 
-    for x, y, d in marbles:
-        nx, ny, d = move(x, y, d)
-        grid[nx][ny] += 1
-        next_marbles_set.add((nx, ny, d))
+                if not self.in_range(_x, _y):
+                    ball.d = d_opposite_dict[ball.d]
+                    _x = x
+                    _y = y
+                
+                if _data[_y][_x].val:
+                    if _data[_y][_x] == "crush":
+                        continue
+                    _data[_y][_x].val = "crush"
+                    continue
 
-    return next_marbles_set
+                _data[_y][_x].val = ball
+        
+        for y in _data:
+            for x in _data[y]:
+                if _data[y][x].val == "crush":
+                    _data[y][x].val = None
+        
+        self.data = _data
 
+    def play(self):
+        for _ in range(self.n * 2):
+            self._play()
 
-def check_dupl(next_marbles_set):
-    global marbles
-    cnt = 0
-    next_marbles = []
-    for x, y, d in next_marbles_set:
-        if grid[x][y] == 1:
-            next_marbles.append((x, y, d))
-            cnt += 1
-            
-    marbles = next_marbles
-    return cnt
+t = int(input())
 
-
-def simulate():
-    for _ in range(2 * grid_size):
-        next_marbles_set = move_all()
-        ans = check_dupl(next_marbles_set)
-        init_grid(next_marbles_set)
-
-    print(ans)
-
+s = ""
 
 for _ in range(t):
-    grid_size, m = tuple(map(int, input().split()))
-    marbles = []
+    n, m = map(int, input().split())
+    ball_grid = BallGrid(n)
 
     for _ in range(m):
-        char_x, char_y, char_dir = tuple(input().split())
-        x, y, d = int(char_x) - 1, int(char_y) - 1, dir_map[char_dir]
-        marbles.append((x, y, d))
+        row, col, d = input().split()
+        [x, y] = int(col) - 1, int(row) - 1
+        ball_grid.init_ball(x, y, d)
+    ball_grid.play()
+    s += str(ball_grid.cnt_ball()) + "\n"
 
-    simulate()
+print(s.strip())
