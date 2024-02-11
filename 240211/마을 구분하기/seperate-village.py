29effcb44869d1ee00cb5450167a9dc1738063a1
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-11 15:42:20 +0900

    Title: 마을 구분하기, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240211/마을 구분하기/seperate-village.py
@@ -0,0 +1,51 @@
+n = int(input())
+grid = [
+    list(map(int,input().split())) for _ in range(n)
+]
+
+visited = [
+    [False] * n for _ in range(n)
+]
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n 
+
+def cango(x,y):
+    if not in_range(x,y):
+        return False
+
+    if visited[x][y]:
+        return False
+    
+    if grid[x][y] == 0:
+        return False
+    
+    return True
+
+peoples = []
+
+def dfs(x,y):
+    global cnt
+    dx = [0,0,-1,1]
+    dy = [-1,1,0,0]
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if cango(nx,ny): #갈 수 있으면 
+            visited[nx][ny] = True
+            cnt += 1
+            dfs(nx,ny)
+
+for i in range(n):
+    for j in range(n):
+        cnt = 0
+        dfs(i,j)
+        if cnt>0:
+            peoples.append(cnt)
+
+print(len(peoples))
+peoples.sort()
+for i in peoples:
+    print(i)
