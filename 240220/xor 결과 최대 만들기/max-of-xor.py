4a2cc1317de239a1d257b4339f856fd2378de47a
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-20 20:11:48 +0900

    Title: xor 결과 최대 만들기, Time: 296ms, Memory: 29MB, Status: Passed - CodeTree

--- 240220/xor 결과 최대 만들기/max-of-xor.py
+++ 240220/xor 결과 최대 만들기/max-of-xor.py
@@ -1,16 +1,30 @@
-from itertools import combinations
-
 n,m=map(int,input().split())
 arr=list(map(int,input().split()))
 
 answer = 0
+take=[]
 
-s = list(combinations(arr,m))
-for x in s:
-    xx=0
-    t = x[0]
-    for i in range(1,len(x)):
-        xx = t ^ x[i]
+def compute():
+    answer=0
+    for elem in take:
+        answer ^= elem
     
-    answer = max(answer,xx)
+    return answer
+
+def xor(cur,cnt):
+    global answer
+
+    # arr에서 m개를 뽑아서 xor 한다.
+    if cur == n:
+        if cnt == m:
+            answer = max(answer, compute())
+        return
+
+    take.append(arr[cur]) #cur 위치의 값을 선택
+    xor(cur+1, cnt+1)
+    take.pop()
+
+    xor(cur+1,cnt) #선택x 
+
+xor(0,0)
 print(answer)
