a99a30d008d8974dcd94a8c7ab190ba3d690b4c2
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-23 14:45:51 +0900

    Title: 비를 피하기, Time: 4506ms, Memory: 30MB, Status: Passed - CodeTree

--- 240223/비를 피하기/stay-out-of-rain.py
+++ 240223/비를 피하기/stay-out-of-rain.py
@@ -1,91 +1,48 @@
 from collections import deque
-import sys
 
-n,h,m = map(int,input().split())
-grid = [
-    list(map(int,input().split()))
-    for _ in range(n)
-]
-
-#0은 이동가능 / 1은 벽 / 2는 사람의 위치 / 3은 목적지
-#각 사람의 위치에서 BFS를 시작해서, 가장 가까운 목적지까지 거리를 계산하면된다.
-#만약 절대 비를 못 피한다면 -1을 출력한다.
-
-step = [
-    [0 for _ in range(n)] for _ in range(n)
-]
-
-visited = [
-    [False for _ in range(n)] for _ in range(n)
-]
-
-def in_range(x,y):
-    return 0<=x<n and 0<=y<n
-
-def cango(x,y):
-    if not in_range(x,y):
-        return False
-
-    if visited[x][y]:
-        return False
-
-    if grid[x][y] == 1:
-        return False #벽이면 못감 (벽만 못감)
-
-    return True
-
+n, h, m = map(int, input().split())     # n: 격자 크기, h: 사람 명수, m: 비를 피할 수 있는 공간
+a = [list(map(int, input().split())) for _ in range(n)] # 0: 이동 가능, 1: 벽이어서 이동 x, 2: 사람, 3: 비를 피할 수 있는 공간
+visited = [[False for _ in range(n)] for _ in range(n)] # 방문 여부
+tmp = [[0 for _ in range(n)] for _ in range(n)]         # 중간 과정 담는 배열
+dirs = ((1,0),(0,1),(-1,0),(0,-1))
+result = [[0 for _ in range(n)] for _ in range(n)]  # 0: 사람이 있던 칸이 아닐 때, -1: 사람이 비를 피할 수 없을 때, 양수: 최소시간
 q = deque()
 
-def push(x,y,s):
-    step[x][y] = s
-    visited[x][y] = True
-    q.append((x,y))
+def initialize():   # 방문 배열과 각 사용자 별 최소시간 초기화
+    for x in range(n):
+        for y in range(n):
+            visited[x][y] = False
+            tmp[x][y] = 0
+def in_range(x,y):
+    return 0 <= x and x < n and 0 <= y and y < n
 
-def clear_visit_step():
-    for i in range(n):
-        for j in range(n):
-            visited[i][j] = False
-            step[i][j] = 0
-
+def can_go(x,y):
+    return in_range(x,y) and (a[x][y] != 1) and not visited[x][y] 
 def bfs():
-    dx = [0,0,-1,1]
-    dy = [-1,1,0,0]
-
+    min_len = []
     while q:
-        x,y = q.popleft()
+        cur = q.popleft()
+        for d in range(4):
+            nx, ny = cur[0]+dirs[d][0], cur[1]+dirs[d][1]
+            if can_go(nx,ny):
+                tmp[nx][ny] = tmp[cur[0]][cur[1]] + 1
+                visited[nx][ny] = True
+                q.append((nx,ny))
+                if a[nx][ny] == 3: min_len.append((tmp[nx][ny]))
+    if min_len == []: return -1
+    return min(min_len)
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if cango(nx,ny):
-                push(nx,ny,step[x][y]+1)
-
-answer=[
-    [0 for _ in range(n)] for _ in range(n)
-]
-
-#시작지점이 h 개
-
-for i in range(n):
-    for j in range(n):
-        clear_visit_step()
-        if grid[i][j] == 2:#사람의 현재 위치 
-            push(i,j,0)
-        bfs()
-        temp = sys.maxsize
-        for x in range(n):
-            for y in range(n):
-                if grid[x][y]==3 and visited[x][y]:
-                    temp = min(temp, step[x][y])
-        
     
-        if temp == 0 or temp == sys.maxsize:
-            answer[i][j]=-1
-        else:   
-            answer[i][j] = temp
 
 for i in range(n):
     for j in range(n):
-        print(answer[i][j],end=' ')
+        if a[i][j] == 2:    # 만약 사람이라면
+            initialize()    # 먼저 초기화 진행
+            visited[i][j] = True    # 방문했다 표기
+            q.append((i,j))
+            result[i][j] = bfs()
+
+for i in range(n):
+    for j in range(n):
+        print(result[i][j], end=' ')
     print()
