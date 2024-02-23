dbc757d3b0713f4a36b221ebbc9c460e8cdbe526
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-23 09:19:37 +0900

    Title: 나이트, Time: 218ms, Memory: 73MB, Status: Wrong Answer - CodeTree

---
+++ 240223/나이트/knight-movements.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+n=int(input())
+r1,c1,r2,c2=map(int,input().split())
+
+r1-=1
+c1-=1
+r2-=1
+c2-=1
+
+q=deque()
+step = [
+    [0 for _ in range(n)] for _ in range(n)
+]
+visited = [
+    [False for _ in range(n)] for _ in range(n)
+]
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def push(x,y,s):
+    step[x][y] = s
+    visited[x][y] = True
+    q.append((x,y))
+
+def bfs():
+    dx = [-2,-1,1,2,2,1,-1,-2]
+    dy = [1,2,2,1,-1,-2,-2,-1]
+
+    while q:
+        x, y = q.popleft()
+
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if in_range(nx,ny) and not visited[nx][ny]:
+                push(nx,ny,step[x][y]+1)
+
+push(r1,c1,0)
+bfs()
+
+if step[r2][c2] == 0:
+    print(-1)
+else:
+    print(step[r2][c2])
