ef4756478b9b08de0933cb53836f1c3cc68783ca
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-28 15:09:28 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240228/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240228/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -12,15 +12,10 @@ for i in card:
 
 answer=0 #A가 이기는 횟수 
 
-#B를 순회하면서
-#B[i]보다 큰 값들을 후보로 넣고
-#후보 중에서 차이가 가장 작은 값을 pick 한뒤에 pop시키고 동시에 answer + 1 한다.
+card_a.sort()
+card_b.sort()
 
 for i in card_b:
-    temp = [k for k in card_a if k > i]
-    if temp:
-        min_diff_value = min(temp, key=lambda x: x - i)
-        card_a.remove(min_diff_value)
+    if card_a[answer] > i:
         answer += 1
-
 print(answer)
