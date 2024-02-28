d5d7cf5f5c5b1e5021e0f121d1f6208cd694f2dc
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-28 12:33:18 +0900

    Title: 숫자 합치기, Time: 334ms, Memory: 33MB, Status: Passed - CodeTree

--- 240228/숫자 합치기/merge-numbers.py
+++ 240228/숫자 합치기/merge-numbers.py
@@ -1,18 +1,23 @@
+import heapq
+
 n=int(input())
 arr=list(map(int,input().split()))
+heap=[]
 
+for i in arr:
+    heapq.heappush(heap,i)
 
 answer=[]
-while len(arr) > 1:
+while len(heap) > 1:
     # 단순하게 그냥 arr을 정렬해뒀으니까
     # 가장 앞에 위치한 2개가 가장 작은 숫자, 즉 더해야하는 숫자다.
-    arr.sort() #가장 작은 2개를 고르기 위해서 정렬한다.
+    
+    a = heapq.heappop(heap)
+    b = heapq.heappop(heap)
 
-    a,b=arr[0],arr[1]
     temp = a+b
     answer.append(temp)
-    arr.pop(0)
-    arr.pop(0)
-    arr.insert(0,temp)
+    
+    heapq.heappush(heap,temp)
 
 print(sum(answer))
