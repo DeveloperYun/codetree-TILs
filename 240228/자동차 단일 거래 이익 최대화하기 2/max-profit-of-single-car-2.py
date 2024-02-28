5fffed4bdff73421a11f35f4cf850d65e11aedf1
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-28 14:21:28 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 144ms, Memory: 31MB, Status: Passed - CodeTree

--- 240228/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240228/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -1,12 +1,21 @@
-n=int(input())
-arr=list(map(int,input().split()))
+def maximize_profit(n, prices):
+    if n <= 1:
+        return 0
 
-answer=0
+    max_profit = 0
+    min_price = prices[0]
 
-for i in range(n-1):
-    for j in range(i+1,n):
-        if arr[i] < arr[j]:
-            answer = max(answer,arr[j]-arr[i])
-        else:
-            continue
-print(answer)
+    for i in range(1, n):
+        current_price = prices[i]
+        max_profit = max(max_profit, current_price - min_price)
+        min_price = min(min_price, current_price)
+
+    return max_profit
+
+# 입력 처리
+n = int(input())
+prices = list(map(int, input().split()))
+
+# 최대 이익 계산 및 출력
+result = maximize_profit(n, prices)
+print(result)
