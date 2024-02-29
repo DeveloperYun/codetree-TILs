07787879f986d35ab776f06d37e0ca6ae1dba9eb
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-29 14:44:06 +0900

    Title: 특별한 문자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240229/특별한 문자/special-character.py
@@ -0,0 +1,20 @@
+x=input()
+
+x = list(x)
+dic={}
+
+for i in x:
+    if i not in dic:
+        dic[i] = 1
+    else:
+        dic[i] += 1
+
+ans=''
+for key, val in dic.items():
+    if val == 1:
+        ans = val
+        break
+if ans=='':
+    print("None")
+else:
+    print(dic[ans])
