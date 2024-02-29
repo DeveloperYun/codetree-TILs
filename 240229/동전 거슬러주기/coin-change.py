8c8791d04832065c643d1d5f57f437b2e4019529
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-02-29 13:55:20 +0900

    Title: 동전 거슬러주기, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240229/동전 거슬러주기/coin-change.py
@@ -0,0 +1,25 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+n,m=map(int,input().split())
+coins=[0]+list(map(int,input().split()))
+
+dp = [0]*(m+1)
+
+for i in range(m+1):
+    dp[i] = INT_MAX
+dp[0]=0
+
+for i in range(1,m+1):
+    for j in range(1,n+1):
+        if i >= coins[j]:
+            if dp[i-coins[j]] == INT_MAX:
+                continue
+            
+            dp[i] = min(dp[i], dp[i-coins[j]]+1)
+
+ans = dp[m]
+if ans == INT_MAX:
+    ans = -1
+print(ans)
