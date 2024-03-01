067fffb2e98e9b973ee203a1448575756968003a
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-03-01 15:09:29 +0900

    Title: 적석과 흑석 2, Time: 4054ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 240301/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240301/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -10,4 +10,20 @@ for _ in range(n):
     a,b = map(int,input().split())
     black.append((a,b))
 
-print(3)
+red.sort()
+black.sort()
+answer=0
+
+idx=0
+while idx<c:
+    num = red[idx]
+
+    for i in range(len(black)):
+        if black[i][0] <= num <= black[i][1]:
+            idx += 1
+            answer+=1
+            black.remove(black[i])
+            break
+    else:
+        idx+=1
+print(answer)
