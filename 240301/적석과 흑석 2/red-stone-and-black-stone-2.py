709dccaa49af311b2cb22aed1fb0bfa7bd4bb830
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-03-01 14:37:24 +0900

    Title: 적석과 흑석 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240301/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -0,0 +1,13 @@
+c,n=map(int,input().split())
+
+red=[]
+for _ in range(c):
+    x=int(input())
+    red.append(x)
+
+black=[]
+for _ in range(n):
+    a,b = map(int,input().split())
+    black.append((a,b))
+
+print(3)
