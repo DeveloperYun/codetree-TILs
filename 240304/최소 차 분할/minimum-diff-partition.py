e16a528686760a151726e97fa8ec9c2aff463a5d
Developer yun <81633639+DeveloperYun@users.noreply.github.com>
2024-03-04 09:50:25 +0900

    Title: 최소 차 분할, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240304/최소 차 분할/minimum-diff-partition.py
@@ -0,0 +1,17 @@
+n=int(input())
+arr=list(map(int,input().split()))
+
+#두 그룹의 합의 차가 최소가 되려면 ?
+'''
+dp[i] = i번째 숫자를 그룹에 포함했을 때. 
+'''
+arr.sort()
+answer=0
+
+for i in range(n-1):
+    temp1 = sum(arr[:i+1])
+    temp2 = sum(arr[i+1:])
+    
+    answer = min(answer, abs(temp1-temp2))
+
+print(answer)
